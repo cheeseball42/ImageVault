b3b25309915b61e06331588346410a0f7a801ed8
Akila <akila.codes@gmail.com>
2026-02-18 17:23:53 +1100

    Initiated front end project with basic login.

---
+++ frontend/src/main.jsx
@@ -0,0 +1,15 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { ChakraProvider, extendTheme } from "@chakra-ui/react"
+import App from './App.jsx'
+
+const theme = extendTheme({});
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
+  </StrictMode>,
+)
